cd8898c9114ea9254814c496065186e37ae63822
Inyoung Maeng <inzero1998@gmail.com>
2025-02-10 13:28:42 +0900

    first commit

---
+++ day1/work1.js
@@ -0,0 +1,26 @@
+//문제1//
+//First Step//
+let A = 30
+let B = 50
+let C = 0
+console.log(A,B,C)
+
+//Second Step//
+a = C
+b = B
+c = A
+console.log(a,b,c)
+
+//Third Step//
+a = B
+b = C
+c = A
+console.log(a,b,c)
+
+//Fourth Step//
+a = B
+b = A
+c = C
+console.log(a,b,c)
+
+//문제2//
---
+++ day1/work2.js
@@ -0,0 +1,25 @@
+let a = 20 + 30
+let b = "20" + "30"
+let c = "Hello" + " " + 2021
+let d = 1 + 2 * 3
+let e = (1 + 3) ** 2
+let f = 1 / 0
+let g = 6 % 2
+let h = 7.5 % 2
+let i = 5 == 5
+let j = 5 === 5
+let k = 5 == "5"
+let l = 5 === "5"
+let m = 5 != 5.0
+let n = 5 !== 5.0
+let o = "true" === true
+let p = 5 <= 5.0
+let q = 5 >= 5
+let r = true || true
+let s = true || false
+let t = true && true
+let u = true && false
+let v = !true
+let w = !false
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w)
