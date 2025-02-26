b07c4089c35a8aed90cd8a2a59cf7383979b8b9e
맹인영 <inzero1998@gmail.com>
2025-02-26 14:44:30 +0900

    study day13(3)

--- day13/script.js
+++ day13/script.js
@@ -1,6 +1,6 @@
 const API_KEY = `80d57d17905645f683713484a9dfbc83`;
 const getLatestNews = async() => {
-    const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
